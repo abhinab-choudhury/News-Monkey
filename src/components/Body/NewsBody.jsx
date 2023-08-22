1eba6bfdf8e5eefcf5dc561f1a9adb31b46e061b
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2023-08-23 04:01:45 +0530

    First commit

---
+++ src/components/Body/NewsBody.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class NewsBody extends Component {
+    ths
+    render() {
+    return (
+      <div className='container-flex'>
+        
+      </div>
+    )
+  }
+}
